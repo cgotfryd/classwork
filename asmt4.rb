f7a80524954d3d78fdeef37dc6a62354addfe6f8
cgotfryd <chasegotfryd@gmail.com>
2019-10-29 21:19:42 -0400

    ammend asmt4 with error checking

--- asmt4.rb
+++ asmt4.rb
@@ -1,8 +1,26 @@
-puts "Please enter the temperature."
-temp = gets.chomp.to_f
-puts "Please enter the temperature unit [C]elcius or [F]arenheit"
-unit = gets.chomp
+invalid = true
 
+while invalid
+	puts "Please enter the temperature."
+	temp = gets.chomp
+	begin
+		temp = Float(temp)
+		invalid = false
+	rescue
+		puts "Invalid input. Please enter a number"
+	end
+end
+
+invalid = true
+while invalid
+	puts "Please enter the temperature unit [C]elcius or [F]arenheit"
+	unit = gets.chomp
+	if unit.downcase == "f" || unit.downcase == "c" then
+		invalid = false
+	else
+		puts "Invalid input. Please enter F or C"
+	end
+end
 if unit.downcase == "f" then
 	result_temp = ((temp-32)*5/9).round(1)
 	result_unit = "C"
